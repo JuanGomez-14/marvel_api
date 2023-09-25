27b75f0a233e688e7eabe199ba239090b4014a14
Juan Manuel Gomez <jmgomezc14@gmail.com>
2023-09-25 00:03:19 -0500

    Se cambio de ironman al apartado general de marvel (se quitó el id ironman). Se añade a el navbar Iron Man (el personaje elejido para el otro punto del trabajo) se termino el index mostrando los personajes con nombre e imagen.

--- js/app.js
+++ js/app.js
@@ -1,22 +1,27 @@
 const marvel = {
   render: () => {
     const urlAPI =
-      "https://gateway.marvel.com:443/v1/public/characters/1009368?ts=1&apikey=9b18d1517056474e8a8bf052d4945580&hash=2f5cf10b13a0810f305a2e86cf39da3d";
-    const container = document.querySelector("#marvel-ironman");
+      "https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=9b18d1517056474e8a8bf052d4945580&hash=2f5cf10b13a0810f305a2e86cf39da3d";
+    const container = document.querySelector("#marvel-row");
     let contentHTML = "";
 
     fetch(urlAPI)
       .then((res) => res.json())
       .then((json) => {
-        console.log(json, "RES.JSON");
+        for (const hero of json.data.results) {
+          let urlHero = hero.urls[0].url;
+          contentHTML += `
+            <div class="col-md-4">
+                <a href="${urlHero}" target="_blank">
+                    <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" class="img-thumbnail">
+                </a>
+                <h3 class="tittle">${hero.name}</h3>
+            </div>
+          `;
+        }
+        container.innerHTML = contentHTML;
       });
   },
 };
 
 marvel.render();
-
-// https://gateway.marvel.com:443/v1/public/characters/1009368?apikey=9b18d1517056474e8a8bf052d4945580
-
-// 12107a24c6198d094793549d9b902317fe91043ba9b18d1517056474e8a8bf052d4945580
-
-// 2F5CF10B13A0810F305A2E86CF39DA3D
