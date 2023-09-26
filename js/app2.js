d7dade852729e18d2cfdcc33e539ccad2495570c
Juan Manuel Gomez <jmgomezc14@gmail.com>
2023-09-26 17:41:07 -0500

    Se añade el total de Stories, Series y Events

--- js/app2.js
+++ js/app2.js
@@ -4,12 +4,22 @@ const marvel = {
       "https://gateway.marvel.com:443/v1/public/characters/1009368?ts=1&apikey=9b18d1517056474e8a8bf052d4945580&hash=2f5cf10b13a0810f305a2e86cf39da3d";
     const urlAPI2 =
       "https://gateway.marvel.com:443/v1/public/characters/1009368/comics?ts=1&apikey=9b18d1517056474e8a8bf052d4945580&hash=2f5cf10b13a0810f305a2e86cf39da3d";
+    const urlAPI3 =
+      "https://gateway.marvel.com:443/v1/public/characters/1009368/events?ts=1&apikey=9b18d1517056474e8a8bf052d4945580&hash=2f5cf10b13a0810f305a2e86cf39da3d";
+    const urlAPI4 =
+      "https://gateway.marvel.com:443/v1/public/characters/1009368/series?ts=1&apikey=9b18d1517056474e8a8bf052d4945580&hash=2f5cf10b13a0810f305a2e86cf39da3d";
+    const urlAPI5 =
+      "https://gateway.marvel.com:443/v1/public/characters/1009368/stories?ts=1&apikey=9b18d1517056474e8a8bf052d4945580&hash=2f5cf10b13a0810f305a2e86cf39da3d";
+
     const container = document.querySelector("#marvel-ironman");
     let contentHTML = "";
     Promise.all([
       fetch(urlAPI1).then((res) => res.json()),
       fetch(urlAPI2).then((res) => res.json()),
-    ]).then(([json1, json2]) => {
+      fetch(urlAPI3).then((res) => res.json()),
+      fetch(urlAPI4).then((res) => res.json()),
+      fetch(urlAPI5).then((res) => res.json()),
+    ]).then(([json1, json2, json3, json4, json5]) => {
       // Procesar los resultados de json1
       for (const hero of json1.data.results) {
         let urlHero = hero.urls[0].url;
@@ -40,7 +50,41 @@ const marvel = {
         </div>
       `;
 
-      // Luego, puedes actualizar el contenido de container con los resultados combinados si es necesario
+      const eventsCount = json3.data.total;
+
+      contentHTML += `
+        <div class="container">
+          <center>
+            <div class="img-ironman">
+              <p class="totalevents">Número de eventos: ${eventsCount}</p>
+            </div>
+          </center>
+        </div>
+      `;
+
+      const seriesCount = json4.data.total;
+
+      contentHTML += `
+        <div class="container">
+          <center>
+            <div class="img-ironman">
+              <p class="totalseries">Número de series: ${seriesCount}</p>
+            </div>
+          </center>
+        </div>
+      `;
+
+      const storiesCount = json5.data.total;
+
+      contentHTML += `
+        <div class="container">
+          <center>
+            <div class="img-ironman">
+              <p class="totalstories">Número de historias: ${storiesCount}</p>
+            </div>
+          </center>
+        </div>
+      `;
       container.innerHTML = contentHTML;
     });
   },
